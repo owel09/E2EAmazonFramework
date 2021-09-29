820526311ef5a656ba34c48415711b9c50b6c7a2
Work <rwlllaguitan@yahoo.com>
2021-09-29 18:09:11 +0800

    May LandingPage na kaso di pa gumagana yung logs

---
+++ src/main/java/LandingPage.java
@@ -0,0 +1,5 @@
+/*
+ *Created by Work on 29/9/2021 6:06 PM
+ */
+public class LandingPage {
+}
--- src/test/java/Homepage.java
+++ src/test/java/Homepage.java
@@ -1,6 +1,9 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 
@@ -11,13 +14,21 @@ public class Homepage extends Base{
 
     WebDriver driver; //for parellel execution kaya declared global ito
 
+    private static Logger logger= LogManager.getLogger(Homepage.class.getName());
+
     @BeforeTest
     public void openURL() throws IOException {
 
         driver = initialiazeDriver(); //pwede mo tawagin yung driver pero ano yung url?
+        logger.info("Driver is initialized");
         driver.get(properties.getProperty("homepageAmazonURL"));
+        logger.info("Navigated to Homepage");
+
+        //nasan yung logs? di lumalabas basta yung logs ko
 
 
 
     }
+
+
 }
